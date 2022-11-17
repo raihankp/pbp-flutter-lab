6694d0cedacb54ab57249436e65ee1521b4265b8
raihan.kus <raihan.kus@ui.ac.id>
2022-11-17 10:54:42 +0700

    Tugas 8

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # counter_7
 
+## Tugas 7
+
 ### Jelaskan apa yang dimaksud dengan stateless widget dan stateful widget dan jelaskan perbedaan dari keduanya.
 - Stateless widget adlaah widget yang dimuat secara statis. Seluruh konfigurasi yang dimuat didalamnya telah diinisiasikan sejak awal widget tersebut dibuat. Stateless widget adalah sebuah widget yang tidak dapat diubah dan tidak akan pernah berubah.
 - Stateful widget merupakan suatu widget yang sifatnya dinamis atau dapat berubah-ubah, kebalikan dari stateless widget. Stateful widget dapat mengubah tampilan, menambah widget, mengubah nilai variabel, icon, warna, dan lain-lain.
@@ -23,3 +25,38 @@ Final harus diinisiasikan terlebih dahulu, tidak bisa diubah valuenya, dan harus
 2. Membuat fungsi untuk mendecrement counter
 3. Menambahkan widget FloatingActionButton kedua untuk mendecrement dan akan hilang apabila counternya = 0
 4. Membuat Text berwarna merah ketika counter % 2 = 0, dan biru ketika tidak terpenuhi. 
+
+## Tugas 8
+
+### Jelaskan perbedaan Navigator.push dan Navigator.pushReplacement.
+- Navigator.push adalah method yang berfungsi untuk push sebuah route ke stack Navigator
+- Navigator.pushReplacement adalah method yang berfungsi untuk mengganti route yg sedang ditamplikan mnjadi route baru, lalu mempush route baru ke Stack Navigator
+
+### Sebutkan widget apa saja yang kamu pakai di proyek kali ini dan jelaskan fungsinya.
+1. Navigator: Mengelola widget child dengan menggunakan struktur stack.
+2. TextFormField: Menerima input user
+3. DropdownButtonFormField: Dropdown untuk memilih input yang diinginkan
+4. Card: Menampilkan data yang telah diinput dalam bentuk cards
+5. Expanded: Memperluas child dari Row, Column, atau Flex untuk mengisi ruang yang tersedia.
+6. Container: Penampung berbagai widget untuk menerapkan posisi, ukuran, dan property lainnya secara seragam
+7. EdgeInsets: Memberikan Offset ke 4 arah kardinal yang bersifat immutable
+8. Visibility: Menampilkan/menyembunyikan suatu child
+
+### Sebutkan jenis-jenis event yang ada pada Flutter (contoh: onPressed).
+onPressed()
+onChanged()
+onTap()
+onSaved()
+
+### Jelaskan bagaimana cara kerja Navigator dalam "mengganti" halaman dari aplikasi Flutter.
+Navigator bekerja dengan menggunakan struktur stack, dimana route yang berada di paling atas stack adalah route yang ditampilkan saat ini. Cara mengganti halamannya adalah dengan menggunakan pop dan push ke stack tersebut.
+
+### Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas.
+1. Membuat widget drawer untuk menavigasi ke route lain
+2. Membuat halaman form yang berisi title budget dan nominal budget dengan tipe data TextField serta type budget dengan DrowdownButtonFormField berisi pengeluaran dan pemasukan
+3. Membuat FloatingActionButton untuk menyimpan input yang sudah diisi dan melakukan validasi kepada input
+4. Menambahkan data yang telah diinput ke dalam list kosong global
+5. Membuat halaman list budget dalam bentuk cards
+6. Import semua file .dart ke masing-masing file
+7. Membuat tampilan setiap elemen pada list dengan menggunakan ListView.builder()
+8. Membuat card yang menampilkan title, nominal, dan type budget
---
+++ lib/form.dart
@@ -0,0 +1,228 @@
+import 'package:counter_7/main.dart';
+import 'package:counter_7/data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+var data = [];
+
+class MyFormPage extends StatefulWidget {
+  const MyFormPage({super.key});
+
+  @override
+  State<MyFormPage> createState() => _MyFormPageState();
+}
+
+class _MyFormPageState extends State<MyFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = "";
+  String _nominal = "";
+  String? type;
+  List<String> _typeList = ['Pemasukan', 'Pengeluaran'];
+
+  var _controllerTitle = TextEditingController();
+  var _nominalController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form Budget'),
+      ),
+      // Drawer menu
+      drawer: Drawer(
+        child: Column(
+          children: [
+            // Menambahkan clickable menu
+            ListTile(
+              title: const Text('counter_7'),
+              onTap: () {
+                // Route menu ke halaman utama
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Tambah Budget'),
+              onTap: () {
+                // Route menu ke halaman form
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Data Budget'),
+              onTap: () {
+                // Route menu ke halaman form
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyDataPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(children: [
+              // Judul Budget
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _controllerTitle,
+                  decoration: InputDecoration(
+                      labelText: "Judul",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      )),
+                  // Menambahkan behavior saat nama diketik
+                  onChanged: (String? value) {
+                    setState(() {
+                      _title = value!;
+                    });
+                  },
+                  // Menambahkan behavior saat data disimpan
+                  onSaved: (String? value) {
+                    setState(() {
+                      _title = value!;
+                    });
+                  },
+                  // Validator sebagai validasi form
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Judul tidak boleh kosong!';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+
+              // Nominal Budget
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _nominalController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                      labelText: "Nominal",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      )),
+                  // Menambahkan behavior saat nama diketik
+                  onChanged: (String? value) {
+                    setState(() {
+                      _nominal = value!;
+                    });
+                  },
+                  // Menambahkan behavior saat data disimpan
+                  onSaved: (String? value) {
+                    setState(() {
+                      _nominal = value!;
+                    });
+                  },
+                  // Validator sebagai validasi form
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nominal tidak boleh kosong!';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+
+              // Type Budget
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 140.0, right: 140.0, bottom: 8.0),
+                child: DropdownButtonFormField(
+                  hint: const Text("Pilih Jenis"),
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  value: type,
+                  items: _typeList.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(items),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      type = newValue ?? "";
+                    });
+                  },
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Pilih salah satu jenis budget!';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ),
+
+      // Simpan
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        padding: const EdgeInsets.all(15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: const Text(
+                "Simpan",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+              ),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  data.add([_title, _nominal, type]);
+                  _controllerTitle.clear();
+                  _nominalController.clear();
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 15,
+                        child: Container(
+                          child: ListView(
+                            padding: const EdgeInsets.only(top: 20, bottom: 20),
+                            shrinkWrap: true,
+                            children: <Widget>[
+                              Center(child: Text('Budget Berhasil Disimpan.')),
+                              SizedBox(height: 20),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Kembali'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
